6bfa99facd2043cab290296134cb0f78ba9debcf
Hugo Arnoult <hugo.arnoult83@gmail.com>
2020-08-12 15:05:13 +0200

    add new review

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,16 +1,17 @@
 class RestaurantsController < ApplicationController
+  # GET / restaurants
   def index
     @restaurants = Restaurant.all
   end
-
+  # GET / restaurants/:id
   def show
     @restaurant = Restaurant.find(params[:id])
   end
-
+  # GET / restaurants/new
   def new
     @restaurant = Restaurant.new
   end
-
+  # POST / restaurants
   def create
     @restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
